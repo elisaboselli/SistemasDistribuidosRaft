b3c76ecf210bc05a1a4515c4fb55903a52435aab
letcharren <leo_etcharren@hotmail.com>
2019-09-28 21:02:53 -0300

    correct Candidate

--- project/src/states/Candidate.java
+++ project/src/states/Candidate.java
@@ -1,12 +1,19 @@
 package states;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+
+import context.Context;
+import messages.HeartBeat;
+import utils.Host;
+
 public class Candidate {
 	
 	static State newtEvent() {
 		return null;
 	}
 
-	static State execute() {
+	static State execute(Context context) {
 		// TODO Auto-generated method stub
 		return null;
 	}
