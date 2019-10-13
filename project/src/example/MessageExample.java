4343168c9160c4162eac113688f5ae01616b02c6
Elisa Boselli <elisaboselli@hotmail.com>
2019-10-13 18:37:09 -0300

    Udp menssages simple example

---
+++ project/src/example/MessageExample.java
@@ -0,0 +1,71 @@
+package example;
+
+import java.net.DatagramPacket;
+
+import com.google.gson.Gson;
+
+//import org.json.simple.JSONObject;
+
+//import org.json.simple.parser.JSONParser;
+//import org.json.simple.parser.ParseException;
+
+public class MessageExample {
+
+    private int term;
+    private String type;
+    private int from;
+    private int to;
+    private String jsonMessage;
+
+    public MessageExample(int term, String type, int from, int to, String json) {
+        this.term = term;
+        this.type = type;
+        this.from = from;
+        this.to = to;
+        this.jsonMessage = json;
+    }
+
+    public int getTerm() {
+        return this.term;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getFrom() {
+        return this.from;
+    }
+
+    public int getTo() {
+        return this.to;
+    }
+
+    public String getJsonMessage() {
+        return this.jsonMessage;
+    }
+
+    public void log(int localPort) {
+        if (localPort == from) {
+            System.out.println("Message Sent to: " + this.to);
+        } else {
+            System.out.println("Message Received from: " + this.from);
+        }
+        System.out.println(this.jsonMessage);
+        System.out.println("\n--------------------------------------------------------------\n");
+
+    }
+
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this, MessageExample.class);
+    }
+
+    public MessageExample fromJson(DatagramPacket request) {
+        byte[] data = new byte[request.getLength()];
+        System.arraycopy(request.getData(), request.getOffset(), data, 0, request.getLength());
+        String jsonStr = new String(data);
+        Gson gson = new Gson();
+        return gson.fromJson(jsonStr, MessageExample.class);
+    }
+}
