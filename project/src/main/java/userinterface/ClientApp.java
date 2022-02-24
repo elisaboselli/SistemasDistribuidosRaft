ad07140034d092aac585f6f219dce6903be5cb75
Elisa Boselli <elisaboselli@hotmail.com>
2022-02-24 16:53:39 -0300

    refactor

--- project/src/main/java/userinterface/ClientApp.java
+++ project/src/main/java/userinterface/ClientApp.java
@@ -5,27 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import utils.Constants;
-import utils.JSONUtils;
-import utils.Message;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
 
 public class ClientApplication2 extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("client-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication2.class.getResource("client-view.fxml"));
         FlowPane flowPane = fxmlLoader.load();
 
         ClientController controller = fxmlLoader.getController();
